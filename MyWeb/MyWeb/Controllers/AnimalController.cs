2354f6cd8f2d222bb9fa1c5c37dc55dc48fb5c67
Marken <nctuan.ityu@gmail.com>
2022-11-11 00:37:36 +0000

    ASP.NET MVC

---
+++ MyWeb/MyWeb/Controllers/AnimalController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MyWeb.Models;
+
+namespace MyWeb.Controllers
+{
+    public class AnimalController : Controller
+    {
+
+
+        private static List<AnimalViewModel> animals = new List<AnimalViewModel>();
+
+        public IActionResult Index()
+        {
+            //AnimalViewModel animal = new AnimalViewModel()
+            return View(animals);
+        }
+
+        public IActionResult Create()
+        {
+            var animal = new AnimalViewModel();
+            return View(animal);
+        }
+
+        public IActionResult CreateAnimal(AnimalViewModel animal)
+        {
+            animals.Add(animal);
+            return RedirectToAction(nameof(Index));
+        }
+       
+        public string Hello()
+        {
+            return "Who's there";
+        }
+    }
+}
+
---
+++ MyWeb/MyWeb/Models/AnimalViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MyWeb.Models
+{
+	public class AnimalViewModel
+	{
+		public string Name { get; set; }
+
+		public int Age { get; set; }
+	}
+}
+
